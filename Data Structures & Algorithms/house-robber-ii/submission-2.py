b0e9189fb0b07118c16569c2417b60a409642f4d
Joey Lin <157548895+joeylin9@users.noreply.github.com>
2026-06-27 12:43:37 -0400

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # take or dont take the last, if dont take, can take first
+        def dfs(i, first_taken, memo):
+            if i >= len(nums):
+                return 0
+            
+            if i == len(nums)-1 and not first_taken:
+                memo[i] = nums[-1]
+            elif i == len(nums)-1 and first_taken:
+                memo[i] = 0
+            
+            if i not in memo:
+                memo[i] = max(nums[i] + dfs(i+2, first_taken, memo), dfs(i+1, first_taken, memo))
+
+            return memo[i]
+        
+        dont_take_first = dfs(1, False, {})
+        take_first = nums[0] + dfs(2, True, {})
+        return max(dont_take_first, take_first)
+        
