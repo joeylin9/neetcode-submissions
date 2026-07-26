71d8a2a84181c5dec967dc3e00f6ffa7216ea911
Joey Lin <157548895+joeylin9@users.noreply.github.com>
2026-07-26 18:57:43 -0400

    Add: longest-increasing-path-in-matrix - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # for each cell, run it
+        # make a dp of every cell of its largest increasing path
+        # make a visited got each run
+        sys.setrecursionlimit(100000)
+
+        def neighbors(coord):
+            dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+            ns = []
+            for d in dirs:
+                i,j = coord[0]+d[0], coord[1]+d[1]
+                if 0<=i<len(matrix) and 0<=j<len(matrix[0]):
+                    ns.append((i,j))
+            return ns
+
+        dp = {} #starting from i,j largest path
+        def dfs(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
+            queue = []
+            for n in neighbors((i,j)):
+                if matrix[n[0]][n[1]] > matrix[i][j]:
+                    queue.append(n)
+            if not queue:
+                return 1
+            
+            dp[(i,j)] = 0
+            for n in queue:
+                dp[(i,j)] = max(dp[(i,j)], 1+dfs(n[0], n[1]))
+            return dp[(i,j)]
+        
+        ans = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                ans = max(ans, dfs(i,j))
+                
+        return ans
+
