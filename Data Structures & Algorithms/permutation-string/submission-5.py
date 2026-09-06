2c8039be09123aa1beb7783dcd0aa4c6a33d82d8
Joey Lin <157548895+joeylin9@users.noreply.github.com>
2026-09-06 00:53:35 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        want = Counter(s1)
+        have = defaultdict(int)
+        correct = 0
+        l = 0
+        for i, c in enumerate(s2):
+            have[c] += 1
+            if have[c] == want[c]:
+                correct += 1
+            elif have[c] == want[c]+1:
+                correct -= 1
+
+            while have[s2[l]] > want[s2[l]]:
+                have[s2[l]] -= 1
+                if have[s2[l]] == want[s2[l]]:
+                    correct += 1
+                l += 1
+                if l >= len(s2):
+                    return False
+            
+            if correct == len(want):
+                return True
+                
+        return False
